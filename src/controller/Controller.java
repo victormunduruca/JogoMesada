d093dd3d4ae3104c802738e6c6bd0523067ea5bd
victormunduruca <victormunduruca@gmail.com>
2017-05-19 09:35:25 -0300

    ControllerJogo -> Controller

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -16,15 +16,17 @@ public class Controller {
 	//Colocar método update para mudar o id do jogador atual com base no controller de rede
 	//Implementar padrão observer com a interface para atualizar quando um jogador muda
 	
-	public int jogar() throws IdNaoEncontradoException {
-		Random rand = new Random();
-		int valorDado = rand.nextInt(6) + 1;
-		
+	public int jogar(int valorDado) throws IdNaoEncontradoException {
 		Jogador jogador = getJogador(idAtual);
 		
+		jogador.setPosicaoPino(jogador.getPosicaoPino() + valorDado);
+		
+		int posicao = jogador.getPosicaoPino();
+		
+		//if(posicao == )
 		
 		
-		return valorDado;
+		return jogador.getId();
 	}
 	
 	public Jogador getJogador(int idJogador) throws IdNaoEncontradoException {
