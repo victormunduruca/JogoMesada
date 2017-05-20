7116278a0b8ee823519c1cf358868cc81216a55c
victormunduruca <victormunduruca@gmail.com>
2017-05-19 23:42:25 -0300

    Rascunho Controller

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -10,24 +10,45 @@ import model.Jogador;
 public class Controller {
 	
 	private ArrayList<Jogador> jogadores; //Jogadores na partida
-	private int idAtual; 
-	private int idJogadorMaquina;
+	private int idAtual; //Id do jogador que está em sua vez
+	private int idJogadorMaquina; //Id jogador desse computador
 	
 	//Colocar método update para mudar o id do jogador atual com base no controller de rede
 	//Implementar padrão observer com a interface para atualizar quando um jogador muda
 	
-	public int jogar(int valorDado) throws IdNaoEncontradoException {
-		Jogador jogador = getJogador(idAtual);
-		
-		jogador.setPosicaoPino(jogador.getPosicaoPino() + valorDado);
-		
-		int posicao = jogador.getPosicaoPino();
-		
-		//if(posicao == )
-		
-		
-		return jogador.getId();
-	}
+	/*
+	 * while(nao acaba o jogo){
+	 * 		if(idAtual == idJogadorMaquina) 
+	 * 				jogadaInterna();
+	 * 		else 
+	 * 				jogadaExterna();	
+	 * }
+	 * Alternativa: while na view
+	 */
+	
+//	public int jogar(int valorDado) throws IdNaoEncontradoException {
+//		
+//		if(idAtual != idJogadorMaquina) {
+//			//
+//		}
+//		Jogador jogador = getJogador(idAtual);
+//		
+//		jogador.setPosicaoPino(jogador.getPosicaoPino() + valorDado);
+//		
+//		int posicao = jogador.getPosicaoPino();
+//		
+//		if(posicao == 2) {
+//			jogador.setSaldo(jogador.getSaldo() + 5000);
+//			//
+//		} 
+////		else if() {
+////			
+////		}
+//			
+//		
+//		
+//		return jogador.getId();
+//	}
 	
 	public Jogador getJogador(int idJogador) throws IdNaoEncontradoException {
 		Iterator<Jogador> it = (Iterator) jogadores.iterator();
