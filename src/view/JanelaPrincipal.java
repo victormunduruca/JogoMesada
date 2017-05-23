21de278a6ac938d066d09d2e46e6d91749b02d91
Franzx41 <dark_jrfsa@hotmail.com>
2017-05-23 04:18:44 -0300

    Adicionado módulo de rede no programa do jogador e também foi adicionado servidor principal. P/ fazer: organizar as pastas

--- src/view/JanelaPrincipal.java
+++ src/view/JanelaPrincipal.java
@@ -1,4 +1,5 @@
 package view;
+
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Font;
@@ -16,12 +17,8 @@ import javax.swing.JPanel;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
-
-
-
 public class JanelaPrincipal {
-	
-	
+
 	private JFrame frame;
 
 	/**
@@ -30,19 +27,14 @@ public class JanelaPrincipal {
 	public static void main(String[] args) {
 		try {
 			// Set System L&F
-			UIManager.setLookAndFeel(
-					UIManager.getSystemLookAndFeelClassName());
-		} 
-		catch (UnsupportedLookAndFeelException e) {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (UnsupportedLookAndFeelException e) {
 			// handle exception
-		}
-		catch (ClassNotFoundException e) {
+		} catch (ClassNotFoundException e) {
 			// handle exception
-		}
-		catch (InstantiationException e) {
+		} catch (InstantiationException e) {
 			// handle exception
-		}
-		catch (IllegalAccessException e) {	       // handle exception
+		} catch (IllegalAccessException e) { // handle exception
 		}
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -67,22 +59,20 @@ public class JanelaPrincipal {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		
-		
-		
-		Pino pinoAmarelo = new Pino("pinoAmarelo.png");
-		Pino pinoAzul = new Pino("pinoAzul.png");
-		Pino pinoRosa = new Pino("pinoRosa.png");
-		Pino pinoRoxo = new Pino("pinoRoxo.png");
-		Pino pinoVerde = new Pino("pinoVerde.png");
-		Pino pinoVermelho = new Pino("pinoVermelho.png");
-		
+
+		final Pino pinoAmarelo = new Pino("pinoAmarelo.png");
+		final Pino pinoAzul = new Pino("pinoAzul.png");
+		final Pino pinoRosa = new Pino("pinoRosa.png");
+		final Pino pinoRoxo = new Pino("pinoRoxo.png");
+		final Pino pinoVerde = new Pino("pinoVerde.png");
+		final Pino pinoVermelho = new Pino("pinoVermelho.png");
+
 		JPanel panel = new JPanel();
 		JLayeredPane jLay = new JLayeredPane();
 		JLabel imagemTabuleiro = new JLabel(new ImageIcon("Tabuleiro.png"));
 		frame = new JFrame();
-		
-		frame.setResizable(false);
+
+		frame.setResizable(true);
 		frame.setLocationRelativeTo(null);
 		frame.setBounds(100, 100, 1190, 871);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -91,7 +81,7 @@ public class JanelaPrincipal {
 
 		jLay.setPreferredSize(new Dimension(896, 685));
 		jLay.add(imagemTabuleiro, new Integer(10));
-		
+
 		jLay.add(pinoAmarelo.getLabel(), new Integer(20));
 		jLay.add(pinoAzul.getLabel(), new Integer(30));
 		jLay.add(pinoRosa.getLabel(), new Integer(40));
@@ -102,10 +92,11 @@ public class JanelaPrincipal {
 		imagemTabuleiro.setBounds(0, 0, 896, 685);
 		frame.getContentPane().add(panel);
 		panel.add(jLay);
-		
-		JButton btnRodarDado = new JButton("Lançar Dado");
+
+		JButton btnRodarDado = new JButton("Jogar Dado");
 		btnRodarDado.addActionListener(new ActionListener() {
 			int valorDado = 0;
+
 			public void actionPerformed(ActionEvent arg0) {
 				valorDado = jogaDado();
 				andaDado(valorDado, pinoAmarelo);
@@ -114,49 +105,56 @@ public class JanelaPrincipal {
 				andaDado(valorDado, pinoVerde);
 				andaDado(valorDado, pinoRoxo);
 				andaDado(valorDado, pinoVermelho);
-				JOptionPane.showMessageDialog(null, "Valor do dado foi de = " + valorDado);
-				System.out.println("Valor dado: " +valorDado);
+				JOptionPane.showMessageDialog(null, "Valor do dado foi de = "
+						+ valorDado);
+				System.out.println("Valor dado: " + valorDado);
 			}
 		});
 		btnRodarDado.setBounds(966, 39, 113, 51);
-		frame.getContentPane().add(btnRodarDado);	
-		
+		frame.getContentPane().add(btnRodarDado);
+
 		JLabel saldo = new JLabel();
-		
+
 		saldo.setFont(new Font("Lucida Sans Typewriter", Font.BOLD, 22));
 		saldo.setBounds(77, 756, 360, 51);
 		frame.getContentPane().add(saldo);
-		
+
 		JButton btnConsultarSaldos = new JButton("Consultar Saldos");
 		btnConsultarSaldos.setBounds(966, 132, 113, 23);
 		frame.getContentPane().add(btnConsultarSaldos);
-		
+
 		JButton btnConsultarCartas = new JButton("Consultar Cartas");
 		btnConsultarCartas.setBounds(966, 179, 113, 23);
 		frame.getContentPane().add(btnConsultarCartas);
 	}
+
 	public void andaDado(int numeroDado, Pino pino) {
-		for(int i = numeroDado; i > 0; i--) {
-			System.out.println("\n i = "+i + "\n");
-			System.out.println("X: " +pino.getX() + " Y: " +pino.getY());
-			if(pino.getX() >= 384 &&  pino.getY() == 137*4) { //chega no final
+		for (int i = numeroDado; i > 0; i--) {
+			System.out.println("\n i = " + i + "\n");
+			System.out.println("X: " + pino.getX() + " Y: " + pino.getY());
+			if (pino.getX() >= 384 && pino.getY() == 137 * 4) { // chega no
+																// final
 				pino.getLabel().setBounds(0, 0, 128, 137);
 				pino.setY(0);
 				pino.setX(0);
 				break;
 			}
-			if(pino.getX() >= 768) { //se for igual ao valor fora da borda ele desce
-				pino.getLabel().setBounds(0, pino.getY()+137, 128, 137);
-				pino.setY(pino.getY()+137);
+			if (pino.getX() >= 768) { // se for igual ao valor fora da borda ele
+										// desce
+				pino.getLabel().setBounds(0, pino.getY() + 137, 128, 137);
+				pino.setY(pino.getY() + 137);
 				pino.setX(0);
-			} else{
+			} else {
 				System.out.println("Entrou no else");
-				pino.getLabel().setBounds(pino.getX() + 128, pino.getY(), 128, 137);
+				pino.getLabel().setBounds(pino.getX() + 128, pino.getY(), 128,
+						137);
 				pino.setX(pino.getX() + 128);
-				System.out.println("DEPOIS X: " +pino.getX() + " Y: " +pino.getY());
+				System.out.println("DEPOIS X: " + pino.getX() + " Y: "
+						+ pino.getY());
 			}
 		}
 	}
+
 	private static int jogaDado() {
 		Random rand = new Random();
 		return rand.nextInt(6) + 1;
